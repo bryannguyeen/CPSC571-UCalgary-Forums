0b339ba3f30190aca925f1d2836f574b71676171
Bryan Nguyen <bryan.khoa97@gmail.com>
2019-11-15 22:47:05 -0700

    Added database schema

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express');
-const promise = require('bluebird');
+const Promise = require('bluebird');
 const sqlite = require('sqlite');
+const SQL = require('sql-template-strings');
 const ejs = require('ejs');
 
 const app = express();
 
-const port = 3000;
-
 app.set('view engine', 'ejs');
 
-// TODO: Set up sqlite database and dbPromise
+let db;
+
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
+async function main() {
+    const port = 3000;
+    db = await sqlite.open('./database.sqlite', { cached: true, Promise }).then(db => db.migrate());
+    await db.run(SQL`PRAGMA foreign_keys = ON`);
+
+    app.listen(port);
+    console.log(`Listening on port ${port}: http://localhost:${port}`);
+}
 
 app.get('/', async (req, res, next) => {
     // TODO: Add frontend pages
-    res.render('pages/index');
+    //res.render('pages/index');
+
+    const user = await req.db.get(SQL`SELECT * FROM user WHERE email = 'bob@gmail.com'`);
+    res.send(user);
+
 });
 
-app.listen(port);
-console.log(`Server listening on port ${port}`);
+main();
---
+++ migrations/001-initial-schema.sql
@@ -0,0 +1,83 @@
+DROP TABLE IF EXISTS "moderating";
+CREATE TABLE IF NOT EXISTS "moderating" (
+	"moderator_email"	varchar(30) NOT NULL,
+	"course_subject"	varchar(4) NOT NULL,
+	"course_number"	INTEGER NOT NULL,
+	"course_semester"	varchar(5) NOT NULL,
+	PRIMARY KEY("moderator_email","course_subject","course_number","course_semester"),
+	FOREIGN KEY("moderator_email") REFERENCES "user"("email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("course_subject","course_number","course_semester") REFERENCES "course"("subject","number","semester") ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "following";
+CREATE TABLE IF NOT EXISTS "following" (
+	"follower_email"	varchar(30) NOT NULL,
+	"course_subject"	varchar(4) NOT NULL,
+	"course_number"	INTEGER NOT NULL,
+	"course_semester"	varchar(5) NOT NULL,
+	PRIMARY KEY("follower_email","course_subject","course_number","course_semester"),
+	FOREIGN KEY("follower_email") REFERENCES "user"("email") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("course_subject","course_number","course_semester") REFERENCES "course"("subject","number","semester") ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "comment";
+CREATE TABLE IF NOT EXISTS "comment" (
+	"comment_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"body_text"	varchar(3000) NOT NULL,
+	"post_date"	date NOT NULL,
+	"post_id"	INTEGER NOT NULL,
+	"poster_email"	varchar(30),
+	FOREIGN KEY("post_id") REFERENCES "post"("post_id") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("poster_email") REFERENCES "user"("email") ON DELETE SET NULL ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "post";
+CREATE TABLE IF NOT EXISTS "post" (
+	"post_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"title"	varchar(25) NOT NULL,
+	"body_text"	varchar(5000) NOT NULL,
+	"poster_email"	varchar(25) DEFAULT NULL,
+	"post_date"	date NOT NULL,
+	"course_subject"	varchar(4) NOT NULL,
+	"course_number"	INTEGER NOT NULL,
+	"course_semester"	varchar(5) NOT NULL,
+	FOREIGN KEY("poster_email") REFERENCES "user"("email") ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY("course_subject") REFERENCES "course"("subject") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("course_number") REFERENCES "course"("number") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("course_semester") REFERENCES "course"("semester") ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "user";
+CREATE TABLE IF NOT EXISTS "user" (
+	"email"	varchar(30) NOT NULL,
+	"username"	varchar(25) NOT NULL,
+	"password"	varchar(40) NOT NULL,
+	"password_salt"	INTEGER NOT NULL,
+	"faculty"	varchar(30) DEFAULT 'Not Specified',
+	PRIMARY KEY("email")
+);
+DROP TABLE IF EXISTS "course";
+CREATE TABLE IF NOT EXISTS "course" (
+	"subject"	varchar(4) NOT NULL,
+	"number"	INTEGER NOT NULL,
+	"semester"	varchar(5) NOT NULL,
+	"professor_id"	INTEGER,
+	PRIMARY KEY("subject","number","semester"),
+	FOREIGN KEY("professor_id") REFERENCES "professor"("professor_id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "review";
+CREATE TABLE IF NOT EXISTS "review" (
+	"review_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"rating"	INTEGER NOT NULL,
+	"description"	varchar(1000),
+	"post_date"	date NOT NULL,
+	"professor_id"	INTEGER NOT NULL,
+	"course_name"	varchar(7),
+	FOREIGN KEY("professor_id") REFERENCES "professor"("professor_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+DROP TABLE IF EXISTS "professor";
+CREATE TABLE IF NOT EXISTS "professor" (
+	"professor_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"firstname"	varchar(25) NOT NULL,
+	"middleinitial"	varchar(5),
+	"lastname"	varchar(25) NOT NULL,
+	"department"	varchar(25) DEFAULT 'Not Specified'
+);
+
+-- Down
